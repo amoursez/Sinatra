ef26460892564dec1b7e91e92fc0f8f87202708b
Sezim <sezim.ydyrysova@alatoo.edu>
2022-01-24 15:53:21 +0600

    fixed error

--- src/Contexts/ProductsContexts.js
+++ src/Contexts/ProductsContexts.js
@@ -1,21 +1,62 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
+import React, { createContext, useReducer } from 'react';
+import axios from 'axios';
+import { API } from '../Helpers/Constants'
 
-export default function Variants() {
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        '& > :not(style)': {
-          m: 1,
-          width: 128,
-          height: 128,
-        },
-      }}
-    >
-      <Paper variant="outlined" />
-      <Paper variant="outlined" square />
-    </Box>
-  );
+export const productContext = createContext()
+
+const INIT_STATE = {
+    products: null
 }
+
+const reducer = (state = INIT_STATE, action) => {
+    switch(action.type){
+        case 'GET_PRODUCTS': 
+        return {
+            ...state, products: action.payload.data,
+            paginatedPages: Math.ceil(action.payload.headers ['x-total-count'] / 3)
+        };
+            
+    }
+} 
+
+const ProductsContextsProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, INIT_STATE)
+    
+    //! Create
+    const addProduct = async (newProduct) => {
+        try{
+            let res = await axios.post(API, newProduct)
+            getProducts()
+            return res
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    //! Read
+    const getProducts = async () => {
+        try{
+           let res = await axios(`${API}${window.location.search}`)
+           let action = {
+               type: 'GET_PRODUCTS',
+               payload: res            
+           }
+           dispatch(action)
+           console.log(res);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return (
+        
+        <productContext.Provider value={{
+            products: state.products,
+            addProduct,
+            getProducts,
+        }}>
+            {children}
+        </productContext.Provider>
+    );
+};
+
+export default ProductsContextsProvider;
