d02d2593255f72b7072f52b2c1b36071d922b58b
Sezim <sezim.ydyrysova@alatoo.edu>
2022-01-25 15:02:39 +0600

    add footer, automatic search, fixed error

--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ProductsContextProvider from './Contexts/ProductsContexts';
 import MyNavbar from './Components/Header/MyNavbar';
+import Footer from './Components/Footer/Footer'
 import Home from './Components/Home/Home';
 import AddProduct from './Components/Admin/AddProduct/AddProduct';
 import EditProduct from './Components/Admin/EditProduct/EditProduct';
@@ -20,6 +21,7 @@ const MyRoutes = () => {
                     <Route path='/' element={<Home />}/>
                     <Route path='/detail/:id' element={<ProductDetail/>} />
                 </Routes>
+                <Footer />
             </BrowserRouter>
         </ProductsContextProvider>
     );
