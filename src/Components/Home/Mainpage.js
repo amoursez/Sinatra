b8547c20194567afbb8a069be1554d31e32902d2
Sezim <sezim.ydyrysova@alatoo.edu>
2022-01-27 15:52:31 +0600

    made main page adaptive and designed

--- src/Components/Home/Mainpage.js
+++ src/Components/Home/Mainpage.js
@@ -2,13 +2,25 @@ import React from 'react';
 import './Mainpage.css'
 import vinyl from '../Home/vinyl sinatra.png'
 import MusicPlayerSlider from './MusicPlayer';
+import { Box } from '@mui/material';
 const Mainpage = () => {
 return(
-    <>
-        <MusicPlayerSlider />
-         <img id="vinyl" src={vinyl}></img>
-    </>
+   
+    <div class = "main">
+
+        <div class="second">
+        <div class = "player">
+        <MusicPlayerSlider/>
+        </div>
+        <h1>We bring lifelong <span class="yellow"> music </span> memories into every home.</h1>
+    
+        
+        </div>
+        <div class = "decoration"> 
+         <img id="vinyl" src={vinyl}></img></div>
+            </div>
     );
+        
 };
 
 export default Mainpage;
